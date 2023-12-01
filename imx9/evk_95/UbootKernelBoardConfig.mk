1da98258decf202bd044f6b11ea399283d0a6c7d
Ji Luo <ji.luo@nxp.com>
2023-12-01 14:11:33 +0800

    MA-21906 imx95: add trusty support
    support trusty on i.MX 95.
    
    Change-Id: I28abb8b1387f57fe8837e80e552658e3f3621e87
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23423
    Reviewed-by: Elven Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23722

--- imx9/evk_95/UbootKernelBoardConfig.mk
+++ imx9/evk_95/UbootKernelBoardConfig.mk
@@ -4,6 +4,7 @@ UBOOT_POST_PROCESS := true
 
 # u-boot target
 TARGET_BOOTLOADER_CONFIG := imx95:imx95_19x19_evk_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx95-trusty-dual:imx95_19x19_evk_android_trusty_dual_defconfig
 TARGET_BOOTLOADER_CONFIG += imx95-evk-uuu:imx95_19x19_evk_android_uuu_defconfig
 
 TARGET_KERNEL_DEFCONFIG := gki_defconfig
