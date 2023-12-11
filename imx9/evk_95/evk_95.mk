256a2955c8b7e77f2bb4e258d48d7900cb9eb10a
Fang Hui <hui.fang@nxp.com>
2023-12-12 00:14:42 +0800

    MA-21972-1 Bring up camera on evk_95
    . add ap1302 firmware ap130x_ar0144_single_fw.bin
    . add mipi-ap1302 dtb imx95-19x19-evk-adv7535-ap1302.dtb
    
    Change-Id: I98454ce6e83d857af94590ffafb0b8e0faa3006d
    Signed-off-by: Fang Hui <hui.fang@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23586
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23724

--- imx9/evk_95/evk_95.mk
+++ imx9/evk_95/evk_95.mk
@@ -292,6 +292,9 @@ PRODUCT_COPY_FILES += \
     $(IMX_DEVICE_PATH)/camera_config_imx95-dual-basler.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/camera_config_imx95-dual-basler.json \
     $(IMX_DEVICE_PATH)/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
+PRODUCT_PACKAGES += \
+    ap130x_ar0144_single_fw.bin
+
 PRODUCT_SOONG_NAMESPACES += hardware/google/camera
 PRODUCT_SOONG_NAMESPACES += vendor/nxp-opensource/imx/camera
 
